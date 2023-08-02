ca8f47714f5acc189d9745e986069c83c065e88b
Mojde Eini <75569963+mojde2@users.noreply.github.com>
2023-08-02 16:39:13 +0330

    Delete djangoBlog directory

--- djangoBlog/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.shortcuts import HttpResponse
-
-def about(request):
-    # return HttpResponse("hi there!I'm hello world!")
-    return render (request,'about.html')
-def home(request):
-    # return HttpResponse('Home')
-    return render (request,'Home.html')
