4fbe7d19e11ea72aa4d0b0d947c5b8bf13508d54
Mojde Eini <75569963+mojde2@users.noreply.github.com>
2023-08-02 16:38:45 +0330

    Delete articles directory

--- articles/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from . import models
-
-def articles_list(request):
-    articles=models.Article.objects.all().order_by('date')
-    args={'articles':articles}
-    return render (request,'articles/articlelist.html',args)
